d0930003382c83f3d5e0f98bb04fd0359dda57fc
swapnilrs <swapnil.salunke@spheregen.com>
2014-08-04 19:16:35 +0530

    First Check-In to Local Repository

---
+++ SampleConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("this is first console app going into GitHub");
+        }
+    }
+}
